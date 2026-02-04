139e2a55cf70a33829001bc2631b16a596ce8e51
bch0w <bryant.h.c@gmail.com>
2026-02-03 17:57:44 -0900

    adding jupyter notebooks for OOP concepts

--- oop/dog_class_example.py
+++ oop/dog_class_example.py
@@ -1,6 +1,7 @@
 """
 An example of a Python Class, which allows implementation of OOP concepts
 """
+from random import randint
 
 class Dog():
     """
@@ -10,11 +11,30 @@ class Dog():
     sound = "woof!"
     energy = 100
 
-    def __init__(self, height, weight, food):
+    def __init__(self, height, weight, food, name=None):
         """Set attributes for your dog"""
         self.height = height
         self.weight = weight
         self.food = food
+        if name is None:
+            self.name = self.assign_name()
+        else:
+            self.name = name
+
+    def __str__(self):
+        """Make Dog talk"""
+        str_out = f"{self.sound}, {self.name} wants to {self.get_state()}"
+        return str_out
+
+    @staticmethod 
+    def assign_name():  # <- Static methods do not require reference to `self`
+        """Provides a randomly assigned name to your doggo"""
+        names = ["Balto", "Lassy", "Spot", "Shadow", "Clifford"]
+        return names[randint(0, 4)]
+
+    @classmethod
+    def bark(cls):
+        print(cls.sound)
 
     def is_tired(self):
         """Check if the dog is tired"""
@@ -37,7 +57,7 @@ class Dog():
         else:
             self.run(distance=5)
 
-    def eat(self, amount_g=25):
+    def eat(self, amount=25):
         """Watch dog eat"""
         if self.food == "dry":
             energy_gained = 1 * amount
@@ -49,20 +69,17 @@ class Dog():
 
         self.energy += energy_gained
 
-    @classmethod
-    def bark(cls):
-        print(cls.sound)
-
+    def get_state(self):
+        """Figure out what Dog wants to do"""
+        if self.is_tired:
+            state = "eat"
+        else:
+            state = "play"
+        return state
 
 if __name__ == "__main__":
-    spot = Dog(height=5, weight=20, food="dry")
-    spot.run(distance=5)
-    spot.eat(amount=10)
-    spot.play()
-    spot.bark()
-
-    breakpoint()
-
-    print(spot.energy)
-        
-        
+    my_dog = Dog(height=5, weight=20, food="dry")
+    my_dog.run(distance=5)
+    my_dog.eat(amount=10)
+    my_dog.play()
+    print(my_dog)
